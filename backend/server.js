2076917dfba1feb774736b13d78580a95c245bed
Alwy <wyalwy.dev@gmail.com>
2021-02-26 08:36:32 +0700

    some changes

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,3 @@
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const path = require("path");
-// const app = express();
-// require("./database");
-
-// app.use(bodyParser.json());
-// app.use(cors());
-
-// // API
-// const exercises = require("/api/exercises");
-// const users = require("/api/users");
-
-// app.use("/api/exercises", exercises);
-// app.use("/api/users", users);
-
-// app.use(express.static(path.join(__dirname, "../build")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../build"));
-// });
-
-// const port = process.env.PORT || 5000;
-// app.listen(port, () => {
-//   console.log(`Server started on port ${port}`);
-// });
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -34,15 +7,42 @@ require("./database");
 
 app.use(bodyParser.json());
 app.use(cors());
+
 // API
+const exercises = require("/api/exercises");
 const users = require("/api/users");
+
+app.use("/api/exercises", exercises);
 app.use("/api/users", users);
 
 app.use(express.static(path.join(__dirname, "../build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build"));
 });
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
+
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const path = require("path");
+// const app = express();
+// require("./database");
+
+// app.use(bodyParser.json());
+// app.use(cors());
+// // API
+// const users = require("/api/users");
+// app.use("/api/users", users);
+
+// app.use(express.static(path.join(__dirname, "../build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../build"));
+// });
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => {
+//   console.log(`Server started on port ${port}`);
+// });
